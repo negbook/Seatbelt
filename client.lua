116b56392ac61ec64491c7b368b4f9eb69c253a4
TehRamsus <50022415+TehRamsus@users.noreply.github.com>
2020-07-14 15:51:10 +0300

    Fixed small mistakes

--- client.lua
+++ client.lua
@@ -18,23 +18,25 @@ AddEventHandler('seatbelt:sounds', function(soundFile, soundVolume)
   })
 end)
 
-IsCar = function(veh)
-        local vc = GetVehicleClass(veh)
-        return (vc >= 0 and vc <= 7) or (vc >= 9 and vc <= 12) or (vc >= 17 and vc <= 20)
-        end 
+function IsCar(veh)
+  local vc = GetVehicleClass(veh)
+  return (vc >= 0 and vc <= 7) or (vc >= 9 and vc <= 12) or (vc >= 17 and vc <= 20)
+end	
 
-Fwv = function (entity)
-        local hr = GetEntityHeading(entity) + 90.0
-        if hr < 0.0 then hr = 360.0 + hr end
-        hr = hr * 0.0174533
-        return { x = math.cos(hr) * 2.0, y = math.sin(hr) * 2.0 }
-      end
+function Fwv(entity)
+  local hr = GetEntityHeading(entity) + 90.0
+  if hr < 0.0 then hr = 360.0 + hr end
+  hr = hr * 0.0174533
+  return { x = math.cos(hr) * 2.0, y = math.sin(hr) * 2.0 }
+end
+
+
  
 Citizen.CreateThread(function()
 	while true do
 	Citizen.Wait(0)
   
-    local ped = GetPlayerPed(-1)
+    local ped = PlayerPedId()
     local car = GetVehiclePedIsIn(ped)
     
     if car ~= 0 and (wasInCar or IsCar(car)) then
@@ -56,12 +58,7 @@ Citizen.CreateThread(function()
 
 
       
-      if speedBuffer[2] ~= nil 
-         and not beltOn
-         and GetEntitySpeedVector(car, true).y > 1.0  
-         and speedBuffer[1] > 19.25 
-         and (speedBuffer[2] - speedBuffer[1]) > (speedBuffer[1] * 0.255) then
-         
+      if not beltOn and speedBuffer[2] ~= nil and GetEntitySpeedVector(car, true).y > 1.0 and speedBuffer[1] > (Config.Speed / 3.6) and (speedBuffer[2] - speedBuffer[1]) > (speedBuffer[1] * 0.255) then
         local co = GetEntityCoords(ped)
         local fw = Fwv(ped)
         SetEntityCoords(ped, co.x + fw.x, co.y + fw.y, co.z - 0.47, true, true, true)
