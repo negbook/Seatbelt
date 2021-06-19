6f91c44d671445f47d9df8e49b8aeb925f434b38
negbook <negbook@gmail.com>
2021-06-19 14:45:21 +0800

    Beauty

--- client.lua
+++ client.lua
@@ -7,22 +7,18 @@ local speedBuffer = {}
 local velBuffer = {}
 local SeatbeltON = false
 local InVehicle = false
-
 function Notify(string)
     SetNotificationTextEntry("STRING")
     AddTextComponentString(string)
     DrawNotification(false, true)
 end
-
 AddEventHandler("seatbelt:sounds",function(soundFile, soundVolume)
     SeatBeltPlaySound(soundFile,soundVolume)
 end)
-
 function IsCar(veh)
     local vc = GetVehicleClass(veh)
     return (vc >= 0 and vc <= 7) or (vc >= 9 and vc <= 12) or (vc >= 17 and vc <= 20)
 end
-
 function Fwv(entity)
     local hr = GetEntityHeading(entity) + 90.0
     if hr < 0.0 then
@@ -31,14 +27,11 @@ function Fwv(entity)
     hr = hr * 0.0174533
     return {x = math.cos(hr) * 2.0, y = math.sin(hr) * 2.0}
 end
-
 CreateThread(function()
     while true do
         Citizen.Wait(0)
-
         local ped = PlayerPedId()
         local car = GetVehiclePedIsIn(ped)
-
         if car ~= 0 and (InVehicle or IsCar(car)) then
             InVehicle = true
             if isUiOpen == false and not IsPlayerDead(PlayerId()) then
@@ -47,15 +40,12 @@ CreateThread(function()
                 end
                 isUiOpen = true
             end
-
             if SeatbeltON then
                 DisableControlAction(0, 75, true) -- Disable exit vehicle when stop
                 DisableControlAction(27, 75, true) -- Disable exit vehicle when Driving
             end
-
             speedBuffer[2] = speedBuffer[1]
             speedBuffer[1] = GetEntitySpeed(car)
-
             if
                 not SeatbeltON and speedBuffer[2] ~= nil and GetEntitySpeedVector(car, true).y > 1.0 and
                     speedBuffer[1] > (Config.Speed / 3.6) and
@@ -68,22 +58,18 @@ CreateThread(function()
                 Citizen.Wait(1)
                 SetPedToRagdoll(ped, 1000, 1000, 0, 0, 0, 0)
             end
-
             velBuffer[2] = velBuffer[1]
             velBuffer[1] = GetEntityVelocity(car)
-
             if IsControlJustReleased(0, Config.Control) and GetLastInputMethod(0) then
                 SeatbeltON = not SeatbeltON
                 if SeatbeltON then
                     Citizen.Wait(1)
-
                     if Config.Sounds then
                         TriggerEvent("seatbelt:sounds", "buckle", Config.Volume)
                     end
                     if Config.Notification then
                         Notify(Config.Strings.seatbelt_on)
                     end
-
                     if Config.Blinker then
                         SetSeatBeltIconOFF()
                     end
@@ -92,11 +78,9 @@ CreateThread(function()
                     if Config.Notification then
                         Notify(Config.Strings.seatbelt_off)
                     end
-
                     if Config.Sounds then
                         TriggerEvent("seatbelt:sounds", "unbuckle", Config.Volume)
                     end
-
                     if Config.Blinker then
                         SetSeatBeltIconON()
                     end
@@ -116,20 +100,17 @@ CreateThread(function()
         end
     end
 end)
-
 CreateThread(function()
     while true do
         Citizen.Wait(10)
         local Vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
         local VehSpeed = GetEntitySpeed(Vehicle) * 3.6
-
         if Config.AlarmOnlySpeed and VehSpeed > Config.AlarmSpeed then
             ShowWindow = true
         else
             ShowWindow = false
             SetSeatBeltIconOFF()
         end
-
         if IsPlayerDead(PlayerId()) or IsPauseMenuActive() then
             if isUiOpen == true then
                 SetSeatBeltIconOFF()
@@ -144,7 +125,6 @@ CreateThread(function()
         end
     end
 end)
-
 CreateThread(function()
     while true do
         Citizen.Wait(3500)
